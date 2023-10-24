9bb726426faa948b67af26a14109e2f3a738aa1d
Nathan Millerberg <nathanm412@gmail.com>
2023-10-24 15:16:51 -0400

    test making changes in react

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  const name = 'Nathan';
+  const greeting = 'Hello';
+  content = <div>{greeting} {name}!</div>;
+  return content;
 }
 
 export default App;
